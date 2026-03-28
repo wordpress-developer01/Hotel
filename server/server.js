532485ac5484f177aacbe3ae4b60b222a25fd5ed
wordpress-developer01 <130246057+wordpress-developer01@users.noreply.github.com>
2026-03-28 21:16:35 +0300

    hello world and p

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js';
-import connectCloudinary from './configs/cloudinary.js';
+import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
 
 const app = express()
